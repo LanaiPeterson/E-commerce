c9badac16564d897342f747893a2f33fb282a892
lanai Your peterson <lanaipeterson@gmail.com>
2025-06-09 16:06:57 -0400

    updated main ts

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,41 @@
 import { fetchProduct } from "./services/apiServices";
 import { calculateDiscount } from "./Utils/discountCalculator";
+import { calculateTax } from "./Utils/taxCalculator";
+
+async function main() {
+    try {
+        const products = await fetchProduct();
+
+        products.forEach(product => {
+            product.displayDetails();
+            const tax = calculateTax(product.price, product.category);
+            const discount = calculateDiscount(product.price, product.discountpercentage);
+
+            console.log(`discount: $${discount}`);
+            console.log(`Tax: $${tax}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main().catch(error => {
+    console.error("An error occurred in the main function:", error);
+});
+
+
+fetchProduct()
+    .then(products => { 
+        products.forEach(product => {
+            product.displayDetails();
+            const tax = calculateTax(product.price, product.category);
+            const discount = calculateDiscount(product.price, product.discountpercentage);
+
+            console.log(`discount: $${discount}`);
+            console.log(`Tax: $${tax}`);
+        });
+    })
+    .catch(error => {
+        console.error("An error occurred while fetching products:", error);
+    }   );
+
